0bb6d6c1d182267bd78283b9e25730b1afdfc17c
sultonqulnd <sultonqulnd@gmail.com>
2025-06-23 16:36:35 +0500

    Fix Vercel build issues: downgrade React to 18, clean ESLint config, remove old files

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,6 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
